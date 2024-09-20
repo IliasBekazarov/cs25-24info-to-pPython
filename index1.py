f670383814139542a4fea81127af3d76ebdeb9cf
IliasBekazarov <iliasbeknazarov4@gmail.com>
2024-09-20 00:44:27 -0700

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,21 @@
+#калкулятор
+a = int(input('1nam'))
+b = int(input('2nam'))
+
+simbol = input('синвол ** + - * / ')
+
+if simbol == '+':
+    print( a + b )
+
+
+if simbol == '-':
+    print( a - b )
+
+if simbol == '*':
+    print( a * b )
+
+if simbol == '/':
+    print( a / b )
+
+if simbol == '**':
+    print( a ** b )
