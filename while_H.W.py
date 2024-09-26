8eb279ed9ba494accea9fba185eaa5397906c840
IliasBekazarov <iliasbeknazarov4@gmail.com>
2024-09-26 05:19:04 -0700

    Add files via upload

---
+++ while_H.W.py
@@ -0,0 +1,16 @@
+# a = int(input('1nam'))
+# b = int(input('2nam'))
+#
+# print(a*b)
+#
+
+a = 1
+
+while a < 10:
+    b = 1
+    while b < 10:
+        print(f'{a}*{b} = {a*b}  ')
+        b *= 3
+    a *= 3
+
+
